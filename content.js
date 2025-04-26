925c275c9234e48e5a1315cb3d3964a2b7924e25
Eduard Fekete <edko.fek@gmail.com>
2025-04-26 17:25:06 +0200

    Revert "[Fix] Prettier code formatting"
    This reverts commit cbf403efa25c38ab66a9dc2e3fad55e42bb87d96.

--- content.js
+++ content.js
@@ -4,56 +4,48 @@ class NormalPlayerObserver {
   //                                                                                                                      > .ytp-chrome-bottom > .ytp-chrome-controls > .ytp-left-controls
 
   /**
-   * @param {(video: HTMLVideoElement, vcLeft: Element)} newPlayerCallback
+   * @param {(video: HTMLVideoElement, vcLeft: Element)} newPlayerCallback 
    */
   constructor(newPlayerCallback) {
     /**
      * @type {(video: HTMLVideoElement, vcLeft: Element)}
      */
-    this._newPlayerCallback = newPlayerCallback;
+    this._newPlayerCallback = newPlayerCallback
 
-    this._find();
+    this._find()
   }
   _find() {
-    if (this._tryIdentify()) return;
+    if (this._tryIdentify()) return
 
-    document.addEventListener(
-      "yt-navigate-finish",
-      this._onNavFinish.bind(this)
-    );
+    document.addEventListener("yt-navigate-finish", this._onNavFinish.bind(this))
 
     // this._observeMutations()
   }
   _tryIdentify() {
-    let video = document.querySelector("video");
-    if (!video) return false;
+    let video = document.querySelector('video')
+    if (!video) return false
 
-    let vcLeft = document.querySelector(".ytp-left-controls");
-    if (!vcLeft) return false;
+    let vcLeft = document.querySelector('.ytp-left-controls')
+    if (!vcLeft) return false
 
-    this._newPlayerCallback(video, vcLeft);
-    return true;
+    this._newPlayerCallback(video, vcLeft)
+    return true
   }
   _onNavFinish() {
-    this._tryIdentify();
+    this._tryIdentify()
   }
   _observeMutations() {
-    this._observer = new MutationObserver(this._onMutation.bind(this));
-    this._observer.observe(document, { childList: true, subtree: true });
+    this._observer = new MutationObserver(this._onMutation.bind(this))
+    this._observer.observe(document, { childList: true, subtree: true })
   }
   _onMutation(mutationList, observer) {
     for (let mutation of mutationList) {
       for (let addedNode of mutation.addedNodes) {
-        if (
-          addedNode.nodeName !== "VIDEO" ||
-          addedNode.nodeName !== "DIV" ||
-          addedNode.className !== "ytp-left-controls"
-        )
-          continue;
+        if (addedNode.nodeName !== 'VIDEO' || addedNode.nodeName !== 'DIV' || addedNode.className !== 'ytp-left-controls') continue
 
         if (this._tryIdentify()) {
-          observer.disconnect();
-          this._observer = null;
+            observer.disconnect()
+            this._observer = null
         }
       }
     }
@@ -73,24 +65,19 @@ class ShortsPlayerObserver {
    */
   constructor(newPlayerCallback) {
     /** @type {(videoElement: HTMLVideoElement, controlsContainer: Element)} */
-    this._newPlayerCallback = newPlayerCallback;
+    this._newPlayerCallback = newPlayerCallback
     /** @type {MutationObserver} */
-    this._observer = new MutationObserver(this._onMutation.bind(this));
-    this._observerVideo = new MutationObserver(
-      this._onVideoMutation.bind(this)
-    );
+    this._observer = new MutationObserver(this._onMutation.bind(this))
+    this._observerVideo = new MutationObserver(this._onVideoMutation.bind(this))
 
-    this._findAndObserve();
-    document.addEventListener(
-      "yt-navigate-finish",
-      this._findAndObserve.bind(this)
-    );
+    this._findAndObserve()
+    document.addEventListener("yt-navigate-finish", this._findAndObserve.bind(this))
   }
 
   _findAndObserve() {
-    let container = document.querySelector("#shorts-inner-container");
-    if (!container) return;
-    this._observer.observe(container, { childList: true });
+    let container = document.querySelector('#shorts-inner-container')
+    if (!container) return
+    this._observer.observe(container, { childList: true })
   }
 
   /**
@@ -98,33 +85,28 @@ class ShortsPlayerObserver {
    */
   _onMutation(mutList, observer) {
     for (let mut of mutList) {
-      if (mut.type !== "childList") continue;
+      if (mut.type !== 'childList') continue
 
       for (let newNode of mut.addedNodes) {
-        if (newNode.nodeName !== "YTD-REEL-VIDEO-RENDERER") continue;
+        if (newNode.nodeName !== 'YTD-REEL-VIDEO-RENDERER') continue
 
-        let playerContainer = newNode.querySelector("#player-container");
-        this._tryPlayerContainer(playerContainer);
+        let playerContainer = newNode.querySelector('#player-container')
+        this._tryPlayerContainer(playerContainer)
       }
     }
   }
 
   _tryPlayerContainer(playerContainer) {
-    let videoElement = playerContainer.querySelector("video");
+    let videoElement = playerContainer.querySelector('video')
     if (!videoElement) {
-      this._observerVideo.observe(playerContainer, { childList: true });
-      return;
+      this._observerVideo.observe(playerContainer, { childList: true })
+      return
     }
 
-    let controlsContainer = playerContainer.querySelector(
-      ".player-controls > ytd-shorts-player-controls"
-    );
-    if (!controlsContainer)
-      throw new Error(
-        "Unexpected: player controls container missing in player container"
-      );
+    let controlsContainer = playerContainer.querySelector('.player-controls > ytd-shorts-player-controls')
+    if (!controlsContainer) throw new Error('Unexpected: player controls container missing in player container')
 
-    this._newPlayerCallback(videoElement, controlsContainer);
+    this._newPlayerCallback(videoElement, controlsContainer)
   }
 
   /**
@@ -132,13 +114,13 @@ class ShortsPlayerObserver {
    */
   _onVideoMutation(mutList, observer) {
     for (let mut of mutList) {
-      if (mut.type !== "childList") continue;
+      if (mut.type !== 'childList') continue
 
       for (let newNode of mut.addedNodes) {
-        if (newNode.id !== "player") continue;
+        if (newNode.id !== 'player') continue
 
-        let playerContainer = newNode.closest("#player-container");
-        this._tryPlayerContainer(playerContainer);
+        let playerContainer = newNode.closest('#player-container')
+        this._tryPlayerContainer(playerContainer)
       }
     }
   }
@@ -146,64 +128,57 @@ class ShortsPlayerObserver {
 
 class Instance {
   /**
-   * @param {HTMLVideoElement} video
-   * @param {Element} controlsContainer
+   * @param {HTMLVideoElement} video 
+   * @param {Element} controlsContainer 
    */
   constructor(video, controlsContainer) {
     /** @type {HTMLVideoElement} */
-    this._video = video;
-    this._controlsContainer = controlsContainer;
+    this._video = video
+    this._controlsContainer = controlsContainer
 
-    this._removeExisting();
-    this._create();
-    this._bind();
-    this._updateRateDisplay();
-    this._updateControlVisibility();
-    this._insert();
+    this._removeExisting()
+    this._create()
+    this._bind()
+    this._updateRateDisplay()
+    this._updateControlVisibility()
+    this._insert()
   }
   _removeExisting() {
-    let existing = this._controlsContainer.querySelector(".pbspeed-container");
-    if (existing) existing.remove();
+    let existing = this._controlsContainer.querySelector('.pbspeed-container')
+    if (existing) existing.remove()
   }
   _create() {
-    let container = document.createElement("div");
-    container.className = "pbspeed-container";
-    container.style =
-      "margin:0 14px; display:flex; align-items: center; gap:12px;";
+    let container = document.createElement('div')
+    container.className = 'pbspeed-container'
+    container.style = 'margin:0 14px; display:flex; align-items: center; gap:12px;'
 
-    let displayHTML = `<div class="rdisplay" style="grid-row: 1; grid-column: 1; font-size:22px; user-select: none;">⏱ <span class="pbspeed-value"></span></div>`;
-    let sliderHTML = `<input id="slider" class="pbspeed-slider" type="range" min="0" max="2" step="0.05" style="grid-row: 1; grid-column: 3; width:7em; height:0.72em; -webkit-appearance:none; outline:none; opacity:0.70; background:#111111; box-shadow: inset 0 0 5px rgba(0, 0, 0, 1); border-radius: 4px;"/>`;
+    let displayHTML = `<div class="rdisplay" style="grid-row: 1; grid-column: 1; font-size:22px; user-select: none;">⏱ <span class="pbspeed-value"></span></div>`
+    let sliderHTML = `<input id="slider" class="pbspeed-slider" type="range" min="0" max="2" step="0.05" style="grid-row: 1; grid-column: 3; width:7em; height:0.72em; -webkit-appearance:none; outline:none; opacity:0.70; background:#111111; box-shadow: inset 0 0 5px rgba(0, 0, 0, 1); border-radius: 4px;"/>`
     // Control layout:
     // | Display | 0.25 0.50 0.75 1.00
     // | Current | 1.25 1.50 1.75 2.00
-    let presetsHTML = `<div class="setrs" style="grid-row: 1; grid-column: 3; display: none; grid-template: 1fr 1fr / repeat(4, auto); column-gap: 6px;"><div>0.25</div><div>0.50</div><div>0.75</div><div>1.00</div><div>1.25</div><div>1.50</div><div>1.75</div><div>2.00</div></div>`;
-    container.innerHTML = `${displayHTML}${sliderHTML}${presetsHTML}`;
+    let presetsHTML = `<div class="setrs" style="grid-row: 1; grid-column: 3; display: none; grid-template: 1fr 1fr / repeat(4, auto); column-gap: 6px;"><div>0.25</div><div>0.50</div><div>0.75</div><div>1.00</div><div>1.25</div><div>1.50</div><div>1.75</div><div>2.00</div></div>`
+    container.innerHTML = `${displayHTML}${sliderHTML}${presetsHTML}`
 
-    this._container = container;
-    this._display = container.querySelector(".rdisplay");
-    this._rateDisplay = this._display.querySelector(".pbspeed-value");
-    this._slider = container.querySelector(".pbspeed-slider");
-    this._presets = container.querySelector(".setrs");
-
+    this._container = container
+    this._display = container.querySelector('.rdisplay')
+    this._rateDisplay = this._display.querySelector('.pbspeed-value')
+    this._slider = container.querySelector('.pbspeed-slider')
+    this._presets = container.querySelector('.setrs')
+    
     // Styling children en-mass
     // container height 48px => element height 48 / 2 = 24 px
-    for (let x of this._presets.childNodes)
-      x.style =
-        "font-size: 14px; line-height: 24px; display: flex; align-items: center; cursor: pointer;";
+    for (let x of this._presets.childNodes) x.style = 'font-size: 14px; line-height: 24px; display: flex; align-items: center; cursor: pointer;'
   }
   _bind() {
-    this._video.addEventListener(
-      "ratechange",
-      this._updateRateDisplay.bind(this)
-    );
+    this._video.addEventListener('ratechange', this._updateRateDisplay.bind(this))
 
-    for (let x of this._presets.childNodes)
-      x.addEventListener("click", this._onPresetClick.bind(this));
-
-    this._slider.addEventListener("input", this._onSliderInput.bind(this));
-
-    this._display.addEventListener("click", this._onRdisplayClick.bind(this));
-    this._display.style.cursor = "pointer";
+    for (let x of this._presets.childNodes) x.addEventListener('click', this._onPresetClick.bind(this))
+  
+    this._slider.addEventListener('input', this._onSliderInput.bind(this))
+    
+    this._display.addEventListener('click', this._onRdisplayClick.bind(this))
+    this._display.style.cursor = 'pointer'
 
     // (How) Can we listen to option changes from a content script?
     // browser.storage.onChanged.addEventListener(e => console.log(e))
@@ -211,40 +186,33 @@ class Instance {
     // browser.storage.local.onChanged.addEventListener(e => console.log(e))
   }
   _updateRateDisplay() {
-    let value = this._video.playbackRate;
-    this._rateDisplay.innerText = `${value.toLocaleString(undefined, {
-      minimumFractionDigits: 2,
-      maximumFractionDigits: 2,
-    })}x`;
-    this._slider.value = value;
+    let value = this._video.playbackRate
+    this._rateDisplay.innerText = `${value.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 })}x`
+    this._slider.value = value
   }
   _onPresetClick(e) {
-    this._video.playbackRate = e.target.innerText;
+    this._video.playbackRate = e.target.innerText
   }
   _onSliderInput(e) {
-    this._video.playbackRate = e.target.value;
+    this._video.playbackRate = e.target.value
   }
   _onRdisplayClick(e) {
-    this._video.playbackRate = 1.0;
+    this._video.playbackRate = 1.0
   }
   async _updateControlVisibility() {
-    let values = await browser.storage.local.get({
-      "show-slider": true,
-      "show-presets": false,
-    });
-    this._presets.style.display = values["show-presets"] ? "grid" : "none";
-    this._slider.style.display = values["show-slider"] ? "block" : "none";
+    let values = await browser.storage.local.get({ 'show-slider': true, 'show-presets': false })
+    this._presets.style.display = values['show-presets'] ? 'grid' : 'none'
+    this._slider.style.display = values['show-slider'] ? 'block' : 'none'
   }
   _insert() {
-    let timeDisplay =
-      this._controlsContainer.querySelector(".ytp-time-display");
+    let timeDisplay = this._controlsContainer.querySelector('.ytp-time-display')
     if (timeDisplay) {
-      timeDisplay.insertAdjacentElement("afterend", this._container);
-      return true;
+      timeDisplay.insertAdjacentElement('afterend', this._container)
+      return true
     }
-
-    this._controlsContainer.appendChild(this._container);
-    return true;
+  
+    this._controlsContainer.appendChild(this._container)
+    return true
   }
 }
 
@@ -253,14 +221,10 @@ let init = async () => {
    * @type {(videoElement: HTMLVideoElement, controlsContainer: Element)}
    */
   let onNewPlayer = (video, controlsContainer) => {
-    console.debug(
-      "[YouTube Playback Speed Control] Identified elements, initializing controls…",
-      video,
-      controlsContainer
-    );
-    new Instance(video, controlsContainer);
-  };
-  new NormalPlayerObserver(onNewPlayer);
-  new ShortsPlayerObserver(onNewPlayer);
-};
-init();
+    console.debug('[YouTube Playback Speed Control] Identified elements, initializing controls…', video, controlsContainer)
+    new Instance(video, controlsContainer)
+  }
+  new NormalPlayerObserver(onNewPlayer)
+  new ShortsPlayerObserver(onNewPlayer)
+}
+init()
--- pages/options.js
+++ pages/options.js
@@ -1,31 +1,12 @@
-let slider = document.getElementById("slider");
-let presets = document.getElementById("presets");
-let maxSpeed = document.getElementById("max-speed");
-let defaultSpeed = document.getElementById("default-speed");
+let slider = document.getElementById('slider')
+let presets = document.getElementById('presets')
 
-slider.addEventListener("change", (e) =>
-  browser.storage.local.set({ "show-slider": e.target.checked })
-);
-presets.addEventListener("change", (e) =>
-  browser.storage.local.set({ "show-presets": e.target.checked })
-);
-maxSpeed.addEventListener("input", (e) =>
-  browser.storage.local.set({ "max-speed": parseFloat(e.target.value) })
-);
-defaultSpeed.addEventListener("input", (e) =>
-  browser.storage.local.set({ "default-speed": parseFloat(e.target.value) })
-);
+slider.addEventListener('change', e => browser.storage.local.set({ 'show-slider': e.target.checked }))
+presets.addEventListener('change', e => browser.storage.local.set({ 'show-presets': e.target.checked }))
 
 async function init() {
-  let values = await browser.storage.local.get({
-    "show-slider": true,
-    "show-presets": false,
-    "max-speed": 3.0,
-    "default-speed": 1.0,
-  });
-  slider.checked = values["show-slider"];
-  presets.checked = values["show-presets"];
-  maxSpeed.value = values["max-speed"];
-  defaultSpeed.value = values["default-speed"];
+    let values = await browser.storage.local.get({ 'show-slider': true, 'show-presets': false })
+    slider.checked = values['show-slider']
+    presets.checked = values['show-presets']
 }
-init();
+init()
